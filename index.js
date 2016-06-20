77873172d5e0d12df85c015a45926c04a0ea75e1
Christopher Dobbins <christopher.dobbins@alumni.vanderbilt.edu>
2016-06-19 22:36:14 -0400

    added functionality to dynamically pull prices from Outbound Trip page

--- index.js
+++ index.js
@@ -13,50 +13,74 @@ let mainPageOptions = {
 // options for GET request to http://us.megabus.com/support/journeyplanner.svc/GetDestinations?originId=<someId>
 
 
-let journeyOptions = {
-    uri: 'http://us.megabus.com/JourneyResults.aspx',
-    qs: {
-      originCode: 289,
-      destinationCode: 290,
-      outboundDepartureDate: '7/03/2016',
-      passengerCount: 1,
-      transportType: 0,
-      concessionCount: 0,
-      nusCount: 0,
-      outboundWheelchairSeated: 0,
-      outboundOtherDisabilityCount: 0,
-      outboundPcaCount: 0,
-      promotionCode: '',
-      withReturn: 0
-    },
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
-    }
-  };
+
 
 function main() {
-requestPromise(mainPageOptions)
+requestPromise(mainPageOptions) // crawls home page to get origin IDs
 .then( (homePage) => {
-  const allOrigins = mapOriginIds(homePage);
+  const allOrigins = mapOriginIds(homePage);  //creates array of city name and id objs
   let originCity = process.argv[2];
-//  let destinationCity = process.argv[3];
+  let destinationCity = process.argv[3];
 //  let outboundDepartureDate = process.argv[4];
 
-  getAvailableDestinations(originCity, allOrigins).then(availableDestinations => {
-   //  console.log(availableDestinations);
-    //journeyOptions.originCode = ;
-    let testDestination = availableDestinations[0];
+// gets the destinations available for the desired origin
+  getAvailableDestinations(originCity, allOrigins).
+  then(availableDestinations => {
+    let testDestination = '';
+    // test block for functionality to pass in destination name
+    // TODO: refactor this
+    availableDestinations.destinations.forEach((destination) => {
+      if (destination.city === destinationCity) {
+        testDestination = destination;
+      }
+      return;
+    });
+    let testDepartureDate = '07/07/2016';
+    let testOrigin = availableDestinations.origin;
+
+    getTripInfo(testOrigin, testDestination, testDepartureDate)
+    .then( (tripInfo) => {
+       let $ = cheerio.load(tripInfo);
+     //   prints price of ticket in '$xx.xx' format
+     //let gridNum = 'l20'; // lowercase L-zero-zero
+     //let gridLine1 = `#JourneyResylts_OutboundList_GridViewResults_ct${gridNum}_row_item li.five`;
+    let i = 0;
+    let gridNum = 'l00';
+    let gridLineId = `#JourneyResylts_OutboundList_GridViewResults_ct${gridNum}_row_item li.five`;
+     while ($(gridLineId).children().eq(0).text()) {
+       gridNum = (i < 10) ? `l0${i}` : `l${i}`;
+       gridLineId = `#JourneyResylts_OutboundList_GridViewResults_ct${gridNum}_row_item li.five`;
+       console.log($(gridLineId).children().eq(0).text().split(/[\s]+/)[2]);
+       i++;
+    }
+    });
   });
 
-
-  // following lines of code call the JourneyResults method
-  // on Megabus's backend
-  // requestPromise(journeyOptions)
-  // .then( (data) => {
-  //   let $ = cheerio.load(data);
-  //   prints price of ticket in '$xx.xx' format
-  //   console.log($('#JourneyResylts_OutboundList_GridViewResults_ctl00_row_item li.five').children().eq(0).text().split(/[\s]+/)[2]);
   });
+}
+
+function getTripInfo(origin, destination, departureDate) {
+  let journeyOptions = {
+  uri: 'http://us.megabus.com/JourneyResults.aspx',
+  qs: {
+    originCode: origin.id,
+    destinationCode: destination.id,
+    outboundDepartureDate: departureDate,
+    passengerCount: 1,
+    transportType: 0,
+    concessionCount: 0,
+    nusCount: 0,
+    outboundWheelchairSeated: 0,
+    outboundOtherDisabilityCount: 0,
+    outboundPcaCount: 0,
+    promotionCode: '',
+    withReturn: 0
+  },
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'
+  }
+};
+   return requestPromise(journeyOptions);
 }
 
 function getAvailableDestinations(originName, allOrigins) {
@@ -67,12 +91,15 @@ function getAvailableDestinations(originName, allOrigins) {
     },
     json: true
   },
-  allAvailableDestinations = [];
+  allAvailableDestinations = {
+    origin: {},
+    destinations: []};
 
   allOrigins.forEach( currCity => {
     if (currCity.city === process.argv[2]) {
       destinationOptions.qs.originId = parseInt(currCity.id);
-      console.log(destinationOptions.qs.originId);
+      allAvailableDestinations.origin.id = destinationOptions.qs.originId;
+      allAvailableDestinations.origin.city = currCity.city;
       return
     }
   });
@@ -80,9 +107,10 @@ function getAvailableDestinations(originName, allOrigins) {
     console.error('invalid origin id; please check the destination name');
     return;
   }
+  // promise that will return the names ('descriptionField') and
+  // ids ('idField') of the destinations available for the city (originName)
   return requestPromise(destinationOptions).then( data => {
-    //console.log(data);
-     allAvailableDestinations = data.d.map( currVal => {
+     allAvailableDestinations.destinations = data.d.map( currVal => {
        let destination = {};
        destination.city = currVal.descriptionField;
        destination.id = currVal.idField;
@@ -93,9 +121,7 @@ function getAvailableDestinations(originName, allOrigins) {
 }
 
 function mapOriginIds(data) {
-  // let data = fs.readFileSync('/home/cj/megabus-crawler/megabus-home.html');
   let $ = cheerio.load(data);
-
   let origins = [];
   let originElements = $('#JourneyPlanner_ddlOrigin').find('option');
   for (let i = 1; i < originElements.length; i++) {

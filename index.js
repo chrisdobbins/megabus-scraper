996fee84fbd9c0c408ffda2076c707ff0fa27695
christopher dobbins <christopherjosephdobbins@gmail.com>
2016-07-04 18:06:09 -0400

    added date to inbound and outbound parts of allTrips obj

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ const requestPromise = require('request-promise'),
     parseTripInfo = require('./parse-trip-info.js');
 
 function main() {
-    const originCity = process.argv[2];
-    const destinationCity = process.argv[3];
+    const originCity = process.argv[2],
+          destinationCity = process.argv[3],
+          inboundDepartureDate = sanitizeDate(process.argv[5]),
+          outboundDepartureDate = sanitizeDate(process.argv[4])
     getAllOriginIds().then( allOrigins => {
         getAvailableDestinations(originCity, allOrigins).
         then(availableDestinations => {
             const destination = verifyDestinationInput(availableDestinations, destinationCity),
-                  origin = availableDestinations.origin,
-                  inboundDepartureDate = sanitizeDate(process.argv[5]),
-                  outboundDepartureDate = sanitizeDate(process.argv[4]);
+                  origin = availableDestinations.origin;
             getTripInfo(origin, destination, outboundDepartureDate, inboundDepartureDate).
             then(tripInfo => {
                 const allTrips = {
-                      outbound: parseTripInfo(tripInfo, 'Outbound'),
-                      inbound: parseTripInfo(tripInfo, 'Inbound')
+                      outbound: {departuredate: outboundDepartureDate,
+                                trips: parseTripInfo(tripInfo, 'Outbound')},
+                      inbound: {departuredate: inboundDepartureDate,
+                                trips: parseTripInfo(tripInfo, 'Inbound')}
                     };
                     console.log(JSON.stringify(allTrips, null, 2));
             });
